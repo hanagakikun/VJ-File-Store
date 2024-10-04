ec71f64add7f790f08636f05276591de06925db9
hanagakikun <sansumabaro046@gmail.com>
2024-10-04 11:14:34 +0530

    new config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ def is_enabled(value, default):
 # Bot Information
 API_ID = int(environ.get("API_ID", "13084577"))
 API_HASH = environ.get("API_HASH", "1c0b20897a5fd2a674db1fa45d4c1a96")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7662664801:AAEfa6JRAeWG2r4C_9EmiKKpL2lnk9tR3rs")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7867859050:AAHqHLeX7evQwkl1IL4Ht89eDYStv80vXtY")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1298304662').split()]
@@ -31,7 +31,7 @@ PORT = environ.get("PORT", "8080")
 CLONE_MODE = bool(environ.get('CLONE_MODE', True)) # Set True or False
 
 # If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://sansumabaro046:15LxLs6Z2Ffm2xFk@cluster0.7pscq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
 CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
 
 # Database Information
