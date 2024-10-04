091409514c255f36c201229ce8efb643bda0a25e
hanagakikun <sansumabaro046@gmail.com>
2024-10-04 19:54:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1298304662').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "Hanagaki_File_Stream_Bot") # without @
+BOT_USERNAME = environ.get("BOT_USERNAME", "animflex_stream_file_bot") # without @
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
@@ -46,7 +46,7 @@ AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
 AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
 
 # Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002474217593"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002303160593"))
 
 # File Caption Information
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
